017b0214c5dade63e85986468c17665f0b77c155
dat <trinhdat0971797927@gmail.com>
2025-09-08 14:20:02 +0700

    chieu8/9

---
+++ products.js
@@ -0,0 +1,336 @@
+
+window.newArrivalProducts =[
+  {
+    id:1,
+    mainImg: "./img/OF07/den.webp",
+    thumbs: [
+      "./img/OF07/den.webp",
+      "./img/OF07/dscf6945.webp",
+      "./img/OF07/nau.webp",
+    ],
+    category: "NEW ARRIVAL",
+    title: "TONKIN CAPTOE OXFORD - OF07",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+ ];
+
+ window.oxfordProducts = [
+  {
+    id:2,
+    mainImg: "./img/OF02/dscf0272.webp",
+    thumbs: [
+      "./img/OF02/dscf0272.webp",
+      "./img/OF02/dscf6967.webp",
+      "./img/OF02/dscf6941.webp",
+    ],
+    category: "OXFORD",
+    title: "CHARLES CAPTOE OXFORD - OF02",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+  {
+     id:3,
+
+    mainImg: "./img/OF32/dscf8054.webp",
+    thumbs: [
+      "./img/OF32/dscf8054.webp",
+      "./img/OF32/dscf9565.webp",
+      "./img/OF32/z4838030916872-882737d7eaf7c8af77deae5b2dbffea2.webp",
+    ],
+    category: "OXFORD",
+    title: "SAVILLE CAPTOE OXFORD - OF32",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+
+   {
+     id:4,
+    mainImg: "./img/OF07/den.webp",
+    thumbs: [
+      "./img/OF07/den.webp",
+      "./img/OF07/dscf6945.webp",
+      "./img/OF07/nau.webp",
+    ],
+    category: "OXFORD",
+    title: "SAVILLE CAPTOE OXFORD - OF32",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+   {
+     id:5,
+    mainImg: "./img/OF-TCCC08/dscf6419-2.webp",
+    thumbs: [
+      "./img/OF-TCCC08/dscf6419-2.webp",
+      "img/OF-TCCC08/dscf7759.webp",
+      "img/OF-TCCC08/dscf6429-d6c70724-5321-453d-adc3-5c4896f6ddff.webp",
+    ],
+    category: "OXFORD",
+    title: "SAVILLE CAPTOE OXFORD - OF32",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+];
+
+ window.loaferProducts = [
+  {
+     id:6,
+    mainImg: "./img/LF20/dscf1271.webp",
+    thumbs: [
+      "./img/LF20/dscf1271.webp",
+      "./img/LF20/dscf1273.webp",
+      "./img/LF20/dscf1275.webp",
+    ],
+    category: "LOAFER",
+    title: "CLASSIC LOAFER - LF20",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+  {
+     id:7,
+    mainImg: "./img/LF18M/dscf1803.webp",
+    thumbs: [
+      "./img/LF18M/dscf1803.webp",
+      "./img/LF18M/dscf1804.webp",
+      "./img/LF18M/dscf2673.webp",
+    ],
+    category: "LOAFER",
+    title: "ELEGANT LOAFER - LF18M",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+    {
+         id:8,
+    mainImg: "img/LF-PF002/dscf8596.webp",
+    thumbs: [
+      "img/LF-PF002/dscf8596.webp",
+      "img/LF-PF002/dscf8601.webp",
+      "img/LF-PF002/dscf8602.webp",
+    ],
+    category: "LOAFER",
+    title: "ELEGANT LOAFER - LF18M",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+    
+  },
+    {
+         id:9,
+    mainImg: "img/PF004/dscf0096.webp",
+    thumbs: [
+      "img/PF004/dscf0096.webp",
+      "img/PF004/dscf8730.webp",
+      "img/PF004/dscf8731-074a4e16-1ad9-4f9e-8f26-4df32f2139c8.webp",
+    ],
+    category: "LOAFER",
+    title: "ELEGANT LOAFER - LF18M",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+      {
+         id:10,
+    mainImg: "img/PF004/dscf0096.webp",
+    thumbs: [
+      "img/PF004/dscf0096.webp",
+      "img/PF004/dscf8730.webp",
+      "img/PF004/dscf8731-074a4e16-1ad9-4f9e-8f26-4df32f2139c8.webp",
+    ],
+    category: "LOAFER",
+    title: "ELEGANT LOAFER - LF18M",
+    priceNew: "1.400.000đ",
+    priceOld: "1.750.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+];
+
+ window.derbyProducts =[
+  {
+     id:11,
+    mainImg: "img/DB07/dscf1507.webp",
+    thumbs: [
+      "img/DB07/dscf1507.webp",
+      "img/DB07/dscf3581.webp",
+      "img/DB07/dscf3587.webp",
+    ],
+    category: "DERBY",
+    title: "CLASSIC DERBY - DB01",
+    priceNew: "1.200.000đ",
+    priceOld: "1.500.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+    {
+         id:12,
+    mainImg: "img/DB14/dscf8725.webp",
+    thumbs: [
+      "img/DB14/dscf8725.webp",
+      "img/DB14/dscf8885.webp",
+      "img/DB14/dscf8897.webp",
+    ],
+    category: "DERBY",
+    title: "CLASSIC DERBY - DB01",
+    priceNew: "1.200.000đ",
+    priceOld: "1.500.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+    {
+         id:13,
+    mainImg: "img/DB24/dscf8708.webp",
+    thumbs: [
+      "img/DB24/dscf8708.webp",
+      "img/DB24/dscf8710.webp",
+      "img/DB24/dscf8713.webp",
+    ],
+    category: "DERBY",
+    title: "CLASSIC DERBY - DB01",
+    priceNew: "1.200.000đ",
+    priceOld: "1.500.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+    {
+         id:14,
+    mainImg: "img/DB34/dscf8882.webp",
+    thumbs: [
+      "img/DB34/dscf8882.webp",
+      "img/DB34/dscf8898.webp",
+      "img/DB34/dscf9534.webp",
+    ],
+    category: "DERBY",
+    title: "CLASSIC DERBY - DB01",
+    priceNew: "1.200.000đ",
+    priceOld: "1.500.000đ",
+    discount: "-20%",
+    tab1: `
+      <p style="text-align: center;"><strong>CHARLES CAPTOE OXFORD - OF02</strong></p>
+      <p style="text-align: center;">Đôi Oxford dáng thon gọn, thoải mái khi di chuyển.</p>
+      <p style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/awItgj53Slw"
+        frameborder="0" allowfullscreen></iframe>
+      </p>
+    `
+  },
+];
+
+
+
+
+
+// ...existing code...
+window.products = [
+  ...(window.newArrivalProducts || []),
+  ...(window.oxfordProducts || []),
+  ...(window.loaferProducts || []),
+  ...(window.derbyProducts || [])
+];
+// ...existing code...
